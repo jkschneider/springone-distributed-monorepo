a22cf9e7dba7f32dc1bdbb610bc29dc4e6aee98f
Jon Schneider <jschneider@pivotal.io>
2018-09-27 09:14:31 -0400

    Atlas collector updates

--- atlas-collector/src/main/java/org/springframework/metrics/atlas/collector/AtlasCollector.java
+++ atlas-collector/src/main/java/org/springframework/metrics/atlas/collector/AtlasCollector.java
@@ -1,38 +1,29 @@
 package org.springframework.metrics.atlas.collector;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.metrics.annotation.Timed;
-import org.springframework.metrics.export.atlas.EnableAtlasMetrics;
-import org.springframework.metrics.instrument.MeterRegistry;
-import org.springframework.metrics.instrument.Timer;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
-@EnableAtlasMetrics
 public class AtlasCollector {
-    public static void main(String[] args) {
-        SpringApplication.run(AtlasCollector.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(AtlasCollector.class, args);
+  }
 }
 
 @RestController
-@Timed
 class TimerController {
-    @Autowired MeterRegistry registry;
-    final Map<String, Timer> timers = new ConcurrentHashMap<>();
+  @Autowired
+  MeterRegistry registry;
 
-    @PostMapping("/api/timer/{name}/{timeNanos}")
-    public void time(@PathVariable String name, @PathVariable Long timeNanos) {
-        timers.computeIfAbsent(name, registry::timer)
-            .record(timeNanos, TimeUnit.NANOSECONDS);
-    }
+  @PostMapping("/api/timer/{name}/{timeNanos}")
+  public void time(@PathVariable String name, @PathVariable Long timeNanos) {
+    registry.timer(name).record(timeNanos, TimeUnit.NANOSECONDS);
+  }
 }
